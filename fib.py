c2916519459d44fff387d8aca4403e66f4a905ad
Bhavini <https://github.com/bhavini2518/Python-Code.git>
2025-12-16 12:22:17 +0530

    secon commit

--- fib.py
+++ fib.py
@@ -1,6 +1,18 @@
-a=1
-b=1
-for i in range(10):
-    c=a+b
-    print(c)
-    a,b=b,c
+# a=1
+# b=1
+# for i in range(10):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+
+def fib(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-1)
+    
+    for i in range(40):
+        print(fib(i))
